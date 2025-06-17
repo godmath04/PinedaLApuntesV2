4299b5a2031be8351cf03a6b9c14b8ae4a190934
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-17 08:03:51 -0500

    Creacion del modelo y los repositorios

--- Repositories/GeolocationRepository.cs
+++ Repositories/GeolocationRepository.cs
@@ -8,5 +8,43 @@ namespace PinedaLApuntesV2.Repositories
 {
     internal class GeolocationRepository
     {
+
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
+        public async Task<Location> GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                return location;
+            }
+         
+            catch (Exception ex)
+            {
+                return new Location { 
+                    Latitude = 46.39,
+                    Longitude = 7.44
+
+                };
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
     }
 }
---
+++ Repositories/WeatherRepository.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using PinedaLApuntesV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PinedaLApuntesV2.Models.WeatherModels.WeatherModel;
+
+namespace PinedaLApuntesV2.Repositories
+{
+    internal class WeatherRepository
+    {
+
+        public async Task<WeatherData> GetWeatherCurrentLocationAsync()
+        {
+
+            GeolocationRepository geolocationRepository = new GeolocationRepository();
+            var location = await geolocationRepository.GetCurrentLocation();
+            return await GetWeatherDataAsync(location.Latitude, location.Longitude);
+        }
+        public async Task<WeatherData> GetWeatherDataAsync(double latitude, double longitude)
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,rain";
+            var response =  await httpClient.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            WeatherData data = JsonConvert.DeserializeObject<WeatherData>(result);
+            return data;
+
+
+        }
+
+    }
+}
